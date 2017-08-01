ad5aea5c8882bf4f5149a1261967d63c9a2b5cd6
Karolina Lubieniecka <lubieniecka@interia.pl>
2017-08-01 16:07:57 +0200

    added commentary to js files

---
+++ 8.4js.js
@@ -0,0 +1,8 @@
+// scripts.js
+
+var name = prompt('Enter your name');
+// my first commentary
+alert('Hello, ' + name);
+/*my second commentary 
+long enough to last two lines*/
+console.log('Hello, ' + name);
